c30dfbc9cef8ea3bf578957e9af837fd77d88e89
Rocio <mprd98@gmail.com>
2023-05-30 01:10:15 -0300

    Do while ejemplo

---
+++ js/loops/do-while.js
@@ -0,0 +1,13 @@
+let edad;
+do {
+    edad = parseInt(prompt("Ingrese la edad de la persona"));
+ } while(edad < 5 || edad > 130)
+
+
+
+//while (edad < 5 || edad > 130) {
+//    console.log("La edad ingesada no es correcta")
+  //  edad = parseInt(prompt("Ingrese la edad de la persona"));
+//}
+
+console.log("Salio del bucle");
--- js/loops/while.js
+++ js/loops/while.js
@@ -1,2 +1,11 @@
 //! WHHILE Y FOR NO SON METODOS ESPECIFICOS DE LOS ARRAY 
-console.log("Bucle While");
+const numero = parseInt(prompt("Ingresar numero del cual quiere obtener la tabla de multiplicacion"));
+
+let i = 11;
+
+while (i <= 10 ) {
+    const multiplicacion = i * numero;
+    console.log (`Resultado de ${i} x ${numero} = ${i * numero}`);
+
+    i++;
+}
